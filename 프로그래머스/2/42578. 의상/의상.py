b6b895d6b2bb03cd50362e54bbd41cd150bee2c0
gawon03 <87633470+gawon03@users.noreply.github.com>
2025-01-25 22:23:02 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,15 +1,13 @@
 def solution(clothes):
-    # 각 종류별 가진 의상을 저장 (종류:[이름, 이름, ...])
-    closet = {} 
-    for name, kind in clothes:
-        if kind in closet.keys():
-            closet[kind] += [name]
-        else:
-            closet[kind] = [name]
+    # 1. 옷을 종류별로 구분하기
+    clothes_type = {}
+    for c, t in clothes:
+        clothes_type[t] = clothes_type.get(t, 0) + 1
     
-    # A의 종류가 N개, B의 종류가 M개 일 때 가능한 모든 경우의 수 (N+1)(M+1)
+    # 2. 입지 않는 경우를 추가하여 모든 조합 계산하기
     answer = 1
-    for _, value in closet.items():
-        answer *= (len(value) + 1)
-        
-    return answer -1
+    for t in clothes_type:
+        answer *= (clothes_type[t]+1)
+    
+    # 3. 아무종류의 옷도 입지 않는 경우 제외하기
+    return answer - 1
