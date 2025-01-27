90d4bc985a39616ed48e6cabf61388e3b31950b9
gawon03 <87633470+gawon03@users.noreply.github.com>
2025-01-27 21:18:36 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,13 +1,10 @@
-# (answer[-1:], (100 - progresses[i])//speeds[i] + 1)
-
-
 def solution(progresses, speeds):
     answer = []
     days = 0
     for p, s in zip(progresses, speeds):
-        if days >= (100 - p)//s + 1*((100 - p)%s!=0):
+        if days >= -((p-100)//s):
             answer[-1] += 1
         else : 
             answer.append(1)
-        days = max(days, (100 - p)//s + 1*((100 - p)%s!=0))
+            days = -((p-100)//s)
     return answer
