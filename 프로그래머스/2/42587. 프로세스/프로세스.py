1419d0e102409369fab20dca4e421d8437dd0838
gawon03 <87633470+gawon03@users.noreply.github.com>
2025-01-29 23:28:07 +0900

    [level 2] Title: 프로세스, Time: 1.23 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,14 +1,16 @@
 from collections import deque
 
 def solution(priorities, location):
-    queue = deque((i,j) for i,j in enumerate(priorities))
-    answer = []
+    queue = deque((i, j) for i, j in enumerate(priorities))
+    answer = [] # 실행된 프로세스
     while queue:
         q = queue.popleft()
+        # 우선순위가 더 높은 프로세스가 하나라도 있으면 큐에 다시 추가 / 아니면 해당 프로세스 실행
         if any(q[1] < p[1] for p in queue):
             queue.append(q)
         else :
             answer.append(q)
+    # location 실행 순서 찾기
     for a in answer:
         if a[0] == location:
             return answer.index(a) + 1
