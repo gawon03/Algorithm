b217dcc57fe3f4e3a88c79c5c06bebbc473619ba
gawon03 <87633470+gawon03@users.noreply.github.com>
2025-01-31 11:30:51 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 10.33 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -6,20 +6,23 @@ def solution(jobs):
     i = 0      # 처리된 작업 개수
     start = -1 # 마지막으로 완료된 작업 시간
     heap = []  # 실행 가능한 작업을 저장하는 최소 힙
+        # jobs에 인덱스 번호 추가 (작업 번호)
+    jobs = [[job[0], job[1], idx] for idx, job in enumerate(jobs)]
+    
     # 모든 작업을 처리할 때까지 반복
     while i < len(jobs): 
         # job 리스트를 하나씩 확인하면서 현재 실행할 수 있는 작업을 힙에 추가
         for job in jobs:             
-            if start < job[0] <= now:  # 이전 작업 완료 이후에 요청된 작업만 추가(이미 처리된 작업 힙추가 방지)
-                heapq.heappush(heap,[job[1],job[0]])
+            if start < job[0] <= now:  # 이전 작업 완료 이후에 요청된 작업만 추가
+                heapq.heappush(heap, [job[1], job[0], job[2]])  # (소요 시간, 요청 시각, 작업 번호)
         
         if heap:
-            current = heapq.heappop(heap)
+            current = heapq.heappop(heap)  # 가장 우선순위가 높은 작업 pop
             start = now   # 작업 수행 시작 시간
-            now += current[0] # 작업 수행 끝난 시간
-            answer += now - current[1] # 요청된 시각부터 현재까지 걸린 시간
-            i += 1 # 처리된 작업 개수 증가
+            now += current[0]  # 작업 수행 끝난 시간
+            answer += now - current[1]  # 요청된 시각부터 현재까지 걸린 시간
+            i += 1  # 처리된 작업 개수 증가
         else:
-            now += 1 # 실행할 작업이 없으면 1 증가
+            now += 1  # 실행할 작업이 없으면 1 증가
             
     return answer // len(jobs)
