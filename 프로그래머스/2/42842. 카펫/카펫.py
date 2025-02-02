8b760d6849fc1d0d37bb52776c733b8bfc2be229
gawon03 <87633470+gawon03@users.noreply.github.com>
2025-02-03 00:13:04 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,9 +1,5 @@
 def solution(brown, yellow):
-    div = []
     for i in range(1, int(yellow**0.5)+1):
         if yellow % i == 0:
-            x = yellow/i
-            y = i
-            div.append([x,y])
-            if 2*(x+y)+4 == brown:
-                return [x+2, y+2]
+            if 2*(yellow/i+i)+4 == brown:
+                return [yellow/i+2, i+2]
