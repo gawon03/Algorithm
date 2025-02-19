fa6045229cfe1c7a866eacb95d060f2afed7dbac
gawon03 <87633470+gawon03@users.noreply.github.com>
2025-02-19 14:25:04 +0900

    [Silver IV] Title: 반복수열, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2331. 반복수열/반복수열.py
+++ 백준/Silver/2331. 반복수열/반복수열.py
@@ -3,7 +3,7 @@ a, p = map(int, input().split())
 array = [a]
 while True:
     num = sum(int(i)**p for i in str(a))
-    if num in set(array):
+    if num in array:
         print(array.index(num))
         break
     array.append(num)
