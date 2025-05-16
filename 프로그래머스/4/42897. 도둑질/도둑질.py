31ad5693ef4743df9ecc937928386862050170e2
gawon03 <87633470+gawon03@users.noreply.github.com>
2025-05-16 21:43:41 +0900

    [level 4] Title: 도둑질, Time: 665.48 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -1,20 +1,21 @@
 def solution(money):
-    temp = [[0, 0], [0, 0]]
-    for i in range(len(money)):
-        if i == 0:
-            new1 = max(temp[0][1], temp[0][0]+money[i])
-            temp[0][0] = temp[0][1]
-            temp[0][1] = new1
-        if i > 0 and i < len(money)-1:
-            new1 = max(temp[0][1], temp[0][0]+money[i])
-            temp[0][0] = temp[0][1]
-            temp[0][1] = new1
-            
-            new2 = max(temp[1][1], temp[1][0]+money[i])
-            temp[1][0] = temp[1][1]
-            temp[1][1] = new2
-        if i == len(money)-1:
-            new2 = max(temp[1][1], temp[1][0]+money[i])
-            temp[1][0] = temp[1][1]
-            temp[1][1] = new2
-    return max(temp[0][1], temp[1][1])
+    answer = 0
+
+    dp1 = [] # 첫 집을 털었을 때
+    dp2 = [] # 첫 집을 안 털고 두 번째 집을 털었을 때
+
+    dp1.append(money[0])
+    dp2.append(0)
+    dp1.append(dp1[0])
+    dp2.append(money[1])
+
+    for i in range(2, len(money) - 1):
+        dp1.append(max(dp1[i - 1], dp1[i - 2] + money[i]))
+        dp2.append(max(dp2[i - 1], dp2[i - 2] + money[i]))
+    e = len(money) - 1
+    dp1.append(dp1[e - 1])
+    dp2.append(max(dp2[e - 1], dp2[e - 2] + money[e]))
+
+    answer = max(dp1[e], dp2[e])
+
+    return answer
