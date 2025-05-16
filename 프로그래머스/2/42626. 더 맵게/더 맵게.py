7dee4ee2605263f37780d20db8b43c7b46e84940
gawon03 <87633470+gawon03@users.noreply.github.com>
2025-05-17 00:04:46 +0900

    [level 2] Title: 더 맵게, Time: 1682.04 ms, Memory: 48.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,16 +1,14 @@
 import heapq
 
 def solution(scoville, K):
+    heapq.heapify(scoville)
     answer = 0
-    mix_scoville = 0
-    heapq.heapify(scoville)         # 최초 리스트에서 힙 정렬
     
-    while scoville[0] < K:          # 스코빌이 기준점 넘어가면 반복 종료
-        if(len(scoville)<2):        # 스코빌 지수 하나 남았을 때 더 이상 불가하므로 -1 리턴
+    while scoville[0] < K:
+        if len(scoville) <= 1:
             return -1
-        mix_scoville = heapq.heappop(scoville) + (heapq.heappop(scoville)*2)
-        heapq.heappush(scoville,mix_scoville)           
-        answer +=1                          
-       # 그 외에는 스코빌 계산하면서 값 빼주고 나온값 push
+        answer += 1
+        mix = heapq.heappop(scoville) + heapq.heappop(scoville)*2
+        heapq.heappush(scoville, mix)
         
     return answer
