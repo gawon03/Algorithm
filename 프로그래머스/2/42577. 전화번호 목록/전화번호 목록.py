654f27408a6fc754620a1f2217091e1c856c63f8
gawon03 <87633470+gawon03@users.noreply.github.com>
2025-05-17 18:17:28 +0900

    [level 2] Title: 전화번호 목록, Time: 88.92 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,11 +1,10 @@
-def solution(phone_book):
-    hash_map = {}
-    for phone_number in phone_book:
-        hash_map[phone_number] = 1
-    for phone_number in phone_book:
-        temp = ""
-        for number in phone_number:
-            temp += number
-            if temp in hash_map and temp != phone_number:
-                return False
+def solution(phoneBook):
+	# 문자열은 첫 글자부터 오름차순으로 정렬됨 ex) '11', '112', '21', '9'
+	# 따라서, 어떤 번호가 다른 번호의 접두어라면 이 둘은 정렬했을 때 앞뒤에 위치함
+    phoneBook = sorted(phoneBook)
+
+	# i번째와 i+1번째를 zip함수로 짝지어서 확인
+    for p1, p2 in zip(phoneBook, phoneBook[1:]):
+        if p2.startswith(p1): # p2가 p1으로 시작하면 False
+            return False
     return True
