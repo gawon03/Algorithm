18261135f731e05b38b136d2c835b4d992fc05d6
gawon03 <87633470+gawon03@users.noreply.github.com>
2025-06-11 14:58:24 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,14 +1,13 @@
 def solution(clothes):
+    # 1. 옷을 종류별로 구분하기
     clothes_type = {}
-    for c, t in clothes :
+    for c, t in clothes:
         clothes_type[t] = clothes_type.get(t, 0) + 1
-        
+    
+    # 2. 입지 않는 경우를 추가하여 모든 조합 계산하기
     answer = 1
     for t in clothes_type:
         answer *= (clothes_type[t]+1)
-        print(t)
-    return answer -1
     
-    
-    
-    return answer
+    # 3. 아무종류의 옷도 입지 않는 경우 제외하기
+    return answer - 1
