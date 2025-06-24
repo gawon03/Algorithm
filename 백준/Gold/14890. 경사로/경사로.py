3c378d6019e8eafccd0cf6d423d536b49618beb0
gawon03 <87633470+gawon03@users.noreply.github.com>
2025-06-24 19:57:47 +0900

    [Gold III] Title: 경사로, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.py
+++ 백준/Gold/14890. 경사로/경사로.py
@@ -1,49 +1,32 @@
-N, L = map(int,input().split())
-pan = []
-for i in range(N):
-    pan.append(list(map(int, input().split())))
-    
-answer = 0
-# 경사로를 놓을 수 없는 경우
-# 경사로를 놓은 곳에 또 경사로를 놓는 경우
-# 낮은 칸과 높은 칸의 높이 차이가 1이 아닌 경우 O
-# 낮은 지점의 칸의 높이가 모두 같지 않거나, L개가 연속되지 않은 경우
-# 경사로를 놓다가 범위를 벗어나는 경우
-def if_can_line(line):
-    bri = [False for _ in range(N)]
+def find(ways):
+    visited = [False]*N
     for i in range(1,N):
-        if abs(line[i-1]-line[i])>1:
+        if abs(ways[i] - ways[i-1]) > 1: # 조건 2
             return False
-        else:
-            if (line[i-1]-line[i])==1: #오른쪽으로 다리 놔야함
-                for j in range(L):
+        
+        if ways[i-1] > ways[i] : # 왼쪽 지점이 더 클 때
+            for j in range(L): # 경사로 크기까지
+                if i + j >= N or ways[i] != ways[i+j] or visited[i+j]: # 경사로가 밖을 벗어나거나, 길이가 다르거나, 이미 설치했다면 (조건 4)
+                    return False
+                if ways[i] == ways[i+j]: # 좌우 높이가 같으면 (조건 1) 
+                    visited[i+j] = True # 경사로 놓자 
                     
-                    if i+j>=N:
-                        return False
-                    if line[i]!=line[i+j]:
-                        return False
-                    if bri[i+j]==True:
-                        return False
-                    if bri[i+j]==False:
-                        bri[i+j]=True
-                    
-            elif (line[i-1]-line[i])==-1: #왼쪽으로 다리 놔야함
-                for j in range(L):
-                    if i-1-j<0:
-                        return False
-                    if line[i-1]!=line[i-1-j]:
-                        return False
-                    if bri[i-1-j]==True:
-                        return False
-                    if bri[i-j-1]==False:
-                        bri[i-j-1]=True
+        elif ways[i-1] < ways[i] : # 오른쪽 지점이 더 클 때
+            for j in range(L):
+                if i - j - 1 < 0 or ways[i-1] != ways[i - j - 1] or visited[i - j - 1]: # 경사로가 밖을 벗어나거나, 길이가 다르거나, 이미 설치했다면 (조건 4)
+                    return False
+                if ways[i-1] == ways[i-j-1]: #조건 1
+                    visited[i-j-1] = True
     return True
 
-for i in range(N):
-    if if_can_line(pan[i]):
-        answer+=1
-for j in range(N):
-    if if_can_line([pan[i][j] for i in range(N)]):
-        answer+=1
 
-print(answer)
+N, L = map(int, input().split())
+way = [list(map(int, input().split())) for _ in range(N)]
+cross = list(zip(*way)) 
+result = 0
+for i in range(N) :
+    if find(way[i]): # 가로
+        result += 1
+    if find(cross[i]): #세로
+        result += 1
+print(result)
