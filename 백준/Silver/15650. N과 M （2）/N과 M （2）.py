c6d95e09ea52db4214dde362cd75ce2fb3961ab2
gawon03 <87633470+gawon03@users.noreply.github.com>
2025-08-12 17:43:24 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split())
+
+def dfs(idx):
+    global ans
+
+    # 종료 조건
+    if len(ans) == M :
+        print(' '.join(map(str, ans)))
+        return
+
+    for i in range(idx, N+1):
+        ans.append(i)
+        dfs(i+1)
+        ans.pop()
+
+ans = []
+dfs(1)
