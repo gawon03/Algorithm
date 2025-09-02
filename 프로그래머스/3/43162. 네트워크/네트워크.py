6123cbfe05deed8b77e938914c400d14ce1e55f3
gawon03 <87633470+gawon03@users.noreply.github.com>
2025-09-02 17:39:02 +0900

    [level 3] Title: 네트워크, Time: 0.27 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,14 +1,15 @@
 def solution(n, computers):
-    visited=[0]*n # 방문 표시 리스트
-    answer=0 # 연결 성분 개수 초기화
-    def dfs(pc): # dfs로 연결된 부분 쭉 탐색
-        visited[pc]=1 # 방문 표시
-        for idx,c in enumerate(computers[pc]): # 해당 컴퓨터에 연결된 컴퓨터 반복
-            if c and visited[idx]==0:
+    visited = [False]*n
+    answer = 0
+    
+    def dfs(pc):
+        visited[pc] = True # 방문 처리
+        for idx, connected in enumerate(computers[pc]): # 연결 정보 하나씩 확인
+            if connected and not visited[idx]: # 연결되어 있고 방문하지 않았다면 이동
                 dfs(idx)
-
-    for pc in range(n):      # n개의 컴퓨터에 대해
-        if visited[pc] == 0: # 방문 안했으면
-            dfs(pc)          # dfs 진행
-            answer+=1        # dfs 1번 진행할때마다 +=1
+    
+    for pc in range(n):
+        if visited[pc] == 0: # 방문하지 않은 PC 확인
+            dfs(pc)
+            answer += 1 # 한 번의 확인(네트워크)마다 1추가
     return answer
