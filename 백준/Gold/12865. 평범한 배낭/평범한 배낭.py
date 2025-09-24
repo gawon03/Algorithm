2e59461ac5d3d0429e1d6d689f798056ececa44d
gawon03 <87633470+gawon03@users.noreply.github.com>
2025-09-24 17:34:15 +0900

    [Gold V] Title: 평범한 배낭, Time: 2456 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -4,8 +4,16 @@ N, K = map(int, input().split())
 # 물건의 무게(W), 가치(V)
 bag = [list(map(int, input().split())) for _ in range(N)]
 
+# 무게 한도가 j일 때 얻을 수 있는 최대 가치 저장
 dp = [0]*(K+1)
+
+# 각 물건에 대해
 for W, V in bag:
-    for j in range(K, W-1, -1):  # 뒤에서 앞으로!
+    # 무게 한도 j를 큰 값부터 작은 값까지 거꾸로 순회
+    for j in range(K, W-1, -1):
+        # 두 값 중 큰 값을 선택
+        # dp[j] : 지금까지 구한 무게 j일 때 최적값
+        # V + dp[j - W] : 새 물건을 담았을 때의 가치
         dp[j] = max(dp[j], V + dp[j - W])
+
 print(dp[K])
